deed2bff5a86e6fdd1b36f0fae855d3fffb29557
Heeryong Kang <drakang4@gmail.com>
2024-12-09 07:08:50 +0900

    Fix model path

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,15 @@
 import "./style.css";
 import "@google/model-viewer";
+import model from "./assets/Chair.webp";
+
+document.querySelector("#ar-viewer")!.innerHTML = `
+  <model-viewer
+    src="${model}"
+    shadow-intensity="1"
+    ar
+    alt="A 3D model carousel"
+    reveal="manual"
+    class="mb-6"
+  >
+  </model-viewer>
+`;
